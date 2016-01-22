f4b42b9664c34bb8acbbc30b4640685a38688f8e
KeyGenQt <keygenqt@gmail.com>
2016-01-22 21:15:46 +0300

    update

--- ImageAjax.php
+++ ImageAjax.php
@@ -2,12 +2,63 @@
 
 namespace keygenqt\imageAjax;
 
+use yii\helpers\Url;
 use yii\widgets\InputWidget;
 
 class ImageAjax extends InputWidget
 {
+    public $url = [];
+    public $defaultLogo;
+    public $btnSelect = 'Select';
+    public $btnDelete = 'Delete';
+    public $subtitle = 'Max size 100 MIB';
+
     public function init()
     {
+        $this->url = Url::toRoute($this->url);
+
+        $id = $this->getId() . rand(0, 999999);
+
+        $baseUrl = ActiveAssets::register($this->getView())->baseUrl;
+
+        echo $this->getView()->render('@keygenqt/imageAjax/views/view', [
+            'id' => $id,
+            'imageDefault' => $this->defaultLogo ? $this->defaultLogo : $baseUrl . '/images/default_logo.jpg',
+            'attribute' => $this->attribute,
+            'model' => $this->model,
+            'subtitle' => $this->subtitle,
+        ]);
+
+        $this->getView()->registerJs("
+//          <script>
+            new Dropzone('#$id-select', {
+                url: '{$this->url}',
+                clickable: true,
+                maxFiles: 1,
+                maxFilesize: 100,
+                thumbnail: function() {},
+                error: function(file, message) {
+                    $('.yii2-image-ajax .error-block').html('Error server response.').show();
+                    setTimeout(function() { $('.yii2-image-ajax .error-block').hide() }, 3000);
+                },
+                success: function(file, response)
+                {
+                    response = JSON.parse(response);
+
+                    if (response.error === false) {
+                        $('#image-{$id}').attr('src', response.url);
+                        $('#$id-delete').show();
+                    } else {
+                        $('.yii2-image-ajax .error-block').html(response.error).show();
+                        setTimeout(function() { $('.yii2-image-ajax .error-block').hide() }, 3000);
+                    }
+                    $('#$id-select').removeClass('img-loading');
+                    $('#$id-hidden-filed').val(response.url);
+                    this.removeAllFiles();
+                }
+            });
+        ");
+
         parent::init();
     }
 }
---
+++ views/view.php
@@ -0,0 +1,60 @@
+<?php
+
+    /** @var String $id */
+    /** @var String $imageDefault */
+    /** @var String $attribute */
+    /** @var String $subtitle */
+    /** @var \yii\base\Model $model */
+
+    use \yii\helpers\Html;
+
+?>
+
+<style>
+    .field-<?= strtolower(preg_replace('/.+\\\(.+)/ui', '$1', $model::className())) . '-' . $attribute ?> label.control-label {
+        display: none;
+    }
+    #<?= $id ?>-select.img-loading .fa-hourglass {
+        color: black;
+        font-size: 12px;
+        display: inline-block;
+        margin-left: 7px;
+    }
+</style>
+
+<div class="yii2-image-ajax <?= $id ?>">
+
+    <?= Html::activeHiddenInput($model, $attribute, array('id' => $id . '-hidden-filed')) ?>
+
+    <div class="table">
+        <div class="table-cell">
+            <div class="image-data">
+                <img id="image-<?= $id ?>" src=" <?= $model->$attribute ? $model->$attribute : $imageDefault ?>"/>
+            </div>
+        </div>
+        <div class="table-cell">
+            <div class="error-block"></div>
+            <div class="btn-buttons-images">
+                <div id="<?= $id ?>-select" class="btn btn-success">Select</div>
+                <?php if (!$model->$attribute):?>
+                    <div id="<?= $id ?>-delete" class="btn btn-danger">Delete</div>
+                <?php endif; ?>
+                <div class="subtitle">
+                    <?= $subtitle ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<script type="text/javascript">
+    <?php if (!$model->$attribute): ?>
+        $('#<?= $id . '-delete' ?>').hide();
+    <?php endif; ?>
+    $('#<?= $id . '-delete' ?>').click(function() {
+        $('#<?= $id . '-hidden-filed' ?>').val("");
+        $('#image-<?= $id ?>').attr('src', "<?= $imageDefault ?>");
+        $(this).hide();
+    });
+</script>
