027458c77bf36bdeb25f328971ec8700567a4945
KeyGenQt <keygenqt@gmail.com>
2016-05-12 15:34:34 +0300

    fix after update

--- ImageAjax.php
+++ ImageAjax.php
@@ -99,13 +99,13 @@ class ImageAjax extends InputWidget
                     $('#{$this->getId()}-select').removeClass('img-loading');
                     $('#{$this->getId()}-hidden-filed').val(response.url);
 
+                    $('#yii2-image-ajax-load').hide();
+
                     sizeFiles{$this->getId()}--;
                     if (sizeFiles{$this->getId()} == 0) {
                         this.removeAllFiles();
+                        afterUpdate{$this->getId()}(response);
                     }
-
-                    $('#yii2-image-ajax-load').hide();
-                    afterUpdate{$this->getId()}(response);
                 }
             });
         ");
